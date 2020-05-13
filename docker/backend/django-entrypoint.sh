60e9316bafd70c36a133e310b5e7fac4c05afff7
yiju <yiju@parasynn.com>
2020-05-13 15:31:35 +0800

    init

---
+++ docker/backend/django-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+until cd /
+do
+    echo "Waiting for django volume..."
+done
+
+until python manage.py migrate --settings=quant_python_platform.settings
+do
+    echo "Waiting for mysql ready..."
+    sleep 2
+done
+
+python manage.py loaddata fixtures.json --settings=quant_python_platform.settings
+python manage.py runserver 0.0.0.0:8000 --settings=quant_python_platform.settings
