c2f7fc96f594c811d9560fc4ca237a61e8f31a59
yiju <yiju@parasynn.com>
2020-05-14 15:40:25 +0800

    可以部署使用

--- docker/backend/django-entrypoint.sh
+++ docker/backend/django-entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-until cd /
+echo `pwd`
+
+until cd /djangoTest
+
+echo `pwd`
+
 do
     echo "Waiting for django volume..."
 done
 
-until python manage.py migrate --settings=quant_python_platform.settings
+python manage.py makemigrations --settings=djangoTest.settings
+until python manage.py migrate --settings=djangoTest.settings
 do
     echo "Waiting for mysql ready..."
     sleep 2
 done
 
-python manage.py loaddata fixtures.json --settings=quant_python_platform.settings
-python manage.py runserver 0.0.0.0:8000 --settings=quant_python_platform.settings
+python manage.py loaddata fixtures.json --settings=djangoTest.settings
+python manage.py runserver 0.0.0.0:8000 --settings=djangoTest.settings
